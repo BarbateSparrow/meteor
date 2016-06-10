cd12e04f87e58a22ba86ad0f8373663978107f18
Dmitriy Voronov <dmv@pc-gep.kyiv.ciklum.net>
2016-06-10 17:43:13 +0300

    init commit

---
+++ client/views/posts/post_item.js
@@ -0,0 +1,10 @@
+/**
+ * Created by dmv on 6/10/16.
+ */
+Template.postItem.helpers({
+    domain: function () {
+        let a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+});
